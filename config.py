967bd9a4cc4df5216706b1bed015b7815a0750e2
Cri <ditta.cinese@gmail.com>
2026-07-15 13:40:58 +0200

    Aggiunta variabile BANCHE a config.py

--- config.py
+++ config.py
@@ -9,3 +9,15 @@ AZIENDA = {
 
 # Legge la password da Render in produzione, o usa il fallback in locale
 PASSWORD_ACCESSO = os.environ.get("ACCESS_PASSWORD", "hu123")
+
+BANCHE = {
+    "BPER": {
+        "id": "BPER",
+        "nome": "BPER Banca di Luserna San Giovanni - IT35S0538730600000004332185",
+        "label_pdf": "BPER Banca di Luserna San Giovanni - IBAN IT35S0538730600000004332185"
+    },
+    "POSTE": {
+        "id": "POSTE",
+        "nome": "Poste Italiane - IT04B0760110200001078221247",
+        "label_pdf": "Poste Italiane - IBAN IT04B0760110200001078221247"
+    }
